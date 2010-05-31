d37770f17b300a8a6be8fa1087f28b9a448a4934
Michael Mellinger <mmellinger66@gmail.com>
2010-05-31 16:47:04 -0400

    Adding a square

--- src/com/example/GLRenderer.java
+++ src/com/example/GLRenderer.java
@@ -16,32 +16,40 @@ import javax.microedition.khronos.opengles.GL10;
 public class GLRenderer implements GLSurfaceView.Renderer {
 
 
-	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-		gl.glShadeModel(GL10.GL_SMOOTH);
-		gl.glClearColor(0, 0, 0, 0);
+    private Square square;
 
-		gl.glClearDepthf(1.0f);
-		gl.glEnable(GL10.GL_DEPTH_TEST);
-		gl.glDepthFunc(GL10.GL_LEQUAL);
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        gl.glShadeModel(GL10.GL_SMOOTH);
+        gl.glClearColor(0, 0, 0, 0);
 
-		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
-	}
+        gl.glClearDepthf(1.0f);
+        gl.glEnable(GL10.GL_DEPTH_TEST);
+        gl.glDepthFunc(GL10.GL_LEQUAL);
 
-	public void onDrawFrame(GL10 gl) {
-		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-		gl.glLoadIdentity();
-	}
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
 
-	public void onSurfaceChanged(GL10 gl, int width, int height) {
-		// avoid division by zero
-		if (height == 0) height = 1;
-		// draw on the entire screen
-		gl.glViewport(0, 0, width, height);
-		// setup projection matrix
-		gl.glMatrixMode(GL10.GL_PROJECTION);
-		gl.glLoadIdentity();
-		GLU.gluPerspective(gl, 45.0f, (float)width / (float)height, 1.0f, 100.0f);
-	}
+        square = new Square();
+
+    }
+
+    public void onDrawFrame(GL10 gl) {
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+        gl.glLoadIdentity();
+
+        gl.glTranslatef(0.0f, -1.2f, -2.0f);    //Move down 1.2 Unit And Into The Screen 6.0
+        square.draw(gl);                                                //Draw the square
+    }
+
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        // avoid division by zero
+        if (height == 0) height = 1;
+        // draw on the entire screen
+        gl.glViewport(0, 0, width, height);
+        // setup projection matrix
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        GLU.gluPerspective(gl, 45.0f, (float) width / (float) height, 1.0f, 100.0f);
+    }
 
 }
 
--- src/com/example/TowerDefense.java
+++ src/com/example/TowerDefense.java
@@ -1,22 +1,34 @@
 package com.example;
 
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.Window;
 
 /**
  * Created by IntelliJ IDEA.
  * User: melling
  * Date: May 31, 2010
  * Time: 3:02:03 PM
- * To change this template use File | Settings | File Templates.
  */
 public class TowerDefense extends Activity {
+
+    private GLSurfaceView surface;
+    private GLRenderer renderer;
+
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+   //     requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        surface = new GLSurfaceView(this);
+        renderer = new GLRenderer();
+        surface.setRenderer(renderer);
+        setContentView(surface);
+//        setContentView(R.layout.main);
     }
 }
