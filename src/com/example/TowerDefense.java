9f40641d729ff0f71ceb8af070d66ad58622b478
Michael Mellinger <mmellinger66@gmail.com>
2010-05-31 15:02:37 -0400

    Created a basic Android app

---
+++ src/com/example/TowerDefense.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: melling
+ * Date: May 31, 2010
+ * Time: 3:02:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TowerDefense extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
